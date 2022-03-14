df0b72ffa9e58623072290a7676e8462d43fa690
Naufal Alexander Suryasumirat <naufal.suryasumirat@gmail.com>
2022-03-14 10:11:42 +0700

    feat: finished tubes_a

--- src/ffnn.py
+++ src/ffnn.py
@@ -72,6 +72,112 @@ class FFNN:
     def get_structure(self) -> tuple((np.array, list)):
         return (self.input_layer, [layer.get_structure() for layer in self.hidden_layers])
 
+# Fungsi membaca input file
+def _input(filename: str, with_input = False) -> tuple((FFNN, np.array, np.array)):
+    f = open(filename, "r")
+    f = f.readlines()
+    f = [line.strip() for line in f]
+
+    nLayer = int(f[0])
+    f = f[1:]
+    n_layer_neurons = []
+    struct_model = {}
+
+    for i in range(nLayer-1):
+        struct_model[i] = {}
+        n_layer_neurons.append(int(f[0]))
+        struct_model[i]["b"] = [float(b) for b in f[1].split()]
+        struct_model[i]["w"] = [[float(w) for w in weights.split()] for weights in f[2:(2 + int(f[0]))]]
+        struct_model[i]["f"] = f[2 + int(f[0])]
+        f = f[2 + int(f[0]) + 1:]
+
+    n_layer_neurons.append(int(f[0]))
+    
+    if (with_input):
+        n_input = int(f[1])
+        f = f[2:]
+        input_data = []
+        for i in range(n_input):
+            input = [int(x) for x in (f[i].split())]
+            input_data.append(input)
+
+        f = f[n_input:]
+        validation_data = []
+        for i in range(n_input):
+            result = [int(y) for y in (f[i].split())]
+            validation_data.append(result)
+
+    model_layers = []
+    for i in range (nLayer-1):
+        weight = struct_model[i]["w"]
+        weight.append(struct_model[i]["b"])
+        layer = Layer(n_layer_neurons[i+1], np.array(weight), struct_model[i]["f"].lower())
+        model_layers.append(layer)
+    
+    if (with_input):
+        return FFNN(model_layers, np.array(input_data)), input_data, validation_data
+    else:
+        return FFNN(model_layers)
+
+# Memperlihatkan koefisien dan struktur dari model
+def showModel(model: FFNN): #masukan berupa FFNN
+    initLayers = model.get_structure()
+
+    countInput = len(initLayers[0])
+    countLayer = len(initLayers[1])
+    if(initLayers[0].ndim == 1):
+        countInput = 1
+    else:
+        countInput = len(initLayers[0])
+    for i in range(0, countInput):
+        if(countInput == 1):
+            inputLayer = initLayers[0]
+    else:
+        inputLayer = initLayers[0][i]
+    output = inputLayer
+    print("==============Data %d==============\n"%(i+1))
+    print("-------------Input------------")
+    print("Input Layer: ", inputLayer)
+    print("------------------------------")
+    for j in range(0, countLayer):
+        weight = initLayers[1][j][1]
+        bias = initLayers[1][j][2]
+        activation = initLayers[1][j][3]
+        combinedArr = initLayers[1][j][4]
+
+        if (j == (countLayer - 1)):
+            print("------ Output Layer ------" )
+            print("Input: ", output)
+            print("Weight: " , weight)
+            print("Bias: " , bias)
+            print('\n')
+            output = activation_functions[activation](np.dot(combinedArr.transpose(), np.append(output, 1)))
+            print("------------------------------")
+            print(inputLayer)
+            print("Predicted Result: ", model.forward(inputLayer))
+        else:
+            print("--- Hidden Layer %d ---" %(j+1))
+            print("Input: ", output)
+            print("H%d Weight: " %(j+1), weight)
+            print("H%d Bias: " %(j+1), bias)
+            print('\n')
+            output = activation_functions[activation](np.dot(combinedArr.transpose(), np.append(output, 1)))
+            print("------------------------------")
+
+# Fungsi menghitung akurasi dari model
+def calculate_accuracy(model: FFNN, input_set, validation_set: list, is_softmax = False):
+    # returns range from 1..100 (percentage)
+    predicted_set = model.predict(np.array(input_set))
+    if (not isinstance(predicted_set, (list, np.ndarray))): return int(predicted_set == validation_set[0]) * 100
+    if (len(predicted_set) != len(validation_set)): return None
+    num_correct = 0
+    for i in range(len(predicted_set)):
+        if is_softmax:
+            if (np.argmax(predicted_set[i]) == validation_set[i]): num_correct += 1
+    else:
+        if predicted_set[i] == validation_set[i]: num_correct += 1
+    return num_correct / len(validation_set) * 100
+
 if __name__ == "__main__":
     # XOR Dataset
     input = np.array([
